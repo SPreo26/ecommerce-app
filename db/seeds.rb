70e430a99efeadd7ed3903badc7fa923f49d1390
spreo260@gmail.com <sergeipreobrahensky@Sergeis-MacBook-Pro.local>
2015-11-25 19:05:45 -0800

    deleted User in the seed file - can't seed users to heroku/postgress for some reason

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,3 @@
-User.create!([
-  {email: "poopman@poo.com", encrypted_password: "$2a$10$sQX3hTfxWGP7jprU0B3SLul453bBAVG8kuxcQ7MXiQcMGXeySduN6", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 4, current_sign_in_at: "2015-11-25 02:33:21", last_sign_in_at: "2015-11-24 02:12:57", current_sign_in_ip: "::1", last_sign_in_ip: "::1"}
-])
 Image.create!([
   {url: "http://ecx.images-amazon.com/images/I/81vPzG13LRL._SL1500_.jpg", product_id: 1},
   {url: "http://annmariebone.com/content/images/thumbs/0000174_rainbow_tree.jpeg", product_id: 2},
